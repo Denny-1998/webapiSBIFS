e50b7ff36aa367662e40a067ec30dd7626d7350a
ChrisOtt0 <sophusott@gmail.com>
2022-11-16 16:24:52 +0100

    Added login functionality, however only returns the privileges so far. A better and more secure solution should be implemented later.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapiSBIFS.Model;
 
 namespace webapiSBIFS.Controllers
 {
-    public class UserController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
-        
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<int>> Login(UserDto request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+            if (user == null)
+                return BadRequest("User not found.");
+
+            if (user.Password != request.Password)
+                return BadRequest("Wrong password.");
+
+            return Ok(user.Privilege);
+        }
     }
 }
--- Model/User.cs
+++ Model/User.cs
@@ -1,4 +1,6 @@
-﻿namespace webapiSBIFS.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapiSBIFS.Model
 {
     public class User
     {
