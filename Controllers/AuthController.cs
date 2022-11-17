f1dbd995e470425ba568ec317fc9e73a161df7d6
ChrisOtt0 <sophusott@gmail.com>
2022-11-17 18:32:44 +0100

    Moved authentication and registration to AuthController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapiSBIFS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public AuthController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<int>> Login(UserDto request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+            if (user == null)
+                return BadRequest("User not found.");
+
+            if (user.Password != request.Password)
+                return BadRequest("Wrong password.");
+
+            return Ok(user.Privilege);
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+            if (user != null)
+                return BadRequest("User account with email already exists.");
+
+            user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Password);
+            if (user != null)
+                return BadRequest("Password taken.");
+
+            User u = new User(request.Email, request.Password);
+
+            await _context.Users.AddAsync(u);
+            await _context.SaveChangesAsync();
+
+            return Ok(request);
+        }
+    }
+}
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -15,35 +15,6 @@ namespace webapiSBIFS.Controllers
             _context = context;
         }
 
-        [HttpPost("Login")]
-        public async Task<ActionResult<int>> Login(UserDto request)
-        {
-            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
-            if (user == null)
-                return BadRequest("User not found.");
-
-            if (user.Password != request.Password)
-                return BadRequest("Wrong password.");
-
-            return Ok(user.Privilege);
-        }
-
-        [HttpPost("Register")]
-        public async Task<ActionResult<User>> Register(UserDto request)
-        {
-            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
-            if (user != null)
-                return BadRequest("User profile with email already exists. Did you forget you password?");
-
-            user = await _context.Users.FirstOrDefaultAsync(u => u.Password == request.Password);
-            if (user != null)
-                return BadRequest("Password already exists.");
-
-            User u = new User(request.Email, request.Password);
-            await _context.AddAsync(u);
-            await _context.SaveChangesAsync();
-
-            return Ok(u);
-        }
+        
     }
 }
