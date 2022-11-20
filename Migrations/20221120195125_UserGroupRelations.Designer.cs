9861fe9a59f22776647ded93f8332a0c280ed6e7
ChrisOtt0 <sophusott@gmail.com>
2022-11-20 20:53:16 +0100

    Added User <-> Group relation and model

--- Migrations/20221120195125_UserGroupRelations.Designer.cs
+++ Migrations/20221120195125_UserGroupRelations.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using webapiSBIFS.Model;
 
@@ -10,9 +11,11 @@ using webapiSBIFS.Model;
 namespace webapiSBIFS.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20221120195125_UserGroupRelations")]
+    partial class UserGroupRelations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Model/Group.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapiSBIFS.Model
+{
+    public class Group
+    {
+        public int GroupID { get; set; }
+        public int OwnerID { get; set; }
+        public List<User>? Participants { get; set; }
+    }
+}
