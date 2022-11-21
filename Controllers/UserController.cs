32d5a0eba876003e69930f98c65a784fe06d483e
ChrisOtt0 <sophusott@gmail.com>
2022-11-21 14:59:21 +0100

    Added Update functionality for Users.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using webapiSBIFS.Model;
+using webapiSBIFS.Tools;
 
 namespace webapiSBIFS.Controllers
 {
@@ -27,12 +28,40 @@ namespace webapiSBIFS.Controllers
                         where u.UserID == userID
                         select u.Email;
 
-            email = query.FirstAsync().Result;
+            email = await query.FirstAsync();
 
             return Ok(new { email });
         }
 
-        //[HttpPut(Name = "Update"), Authorize(Roles = "user")]
+        [HttpPut(Name = "Update"), Authorize(Roles = "user")]
+        public async Task<ActionResult> Update(UserDto request)
+        {
+            string hashedPass = string.Empty;
+            int userID = _userService.GetUserID();
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserID == userID);
+            
+            if (request.Email != null && request.Email != string.Empty)
+            {
+                var isUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+                if (isUser != null)
+                    return UnprocessableEntity("User with given email already exists. Could not update the account.");
 
+                user.Email = request.Email;
+            }
+            if (request.Password != null && request.Password != string.Empty)
+            {
+                string salt = new SaltAdapter().GetSalt();
+                hashedPass = SecurityTools.HashString(request.Password, salt);
+
+                var isUser = await _context.Users.FirstOrDefaultAsync(u => u.Password == hashedPass);
+                if (isUser != null)
+                    return UnprocessableEntity("Password not valid.");
+
+                user.Password = hashedPass;
+            }
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
