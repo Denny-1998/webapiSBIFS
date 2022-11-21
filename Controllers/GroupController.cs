766d6a62d2752c6b4b8cf05740a7c71df290f5a5
ChrisOtt0 <sophusott@gmail.com>
2022-11-21 15:40:46 +0100

    Setup base GroupController

---
+++ Controllers/GroupController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapiSBIFS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly IUserService _userService;
+
+        public GroupController(DataContext context, IUserService userService)
+        {
+            _context = context;
+            _userService = userService;
+        }
+
+
+    }
+}
