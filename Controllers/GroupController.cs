2c713076287da6fba8ea4b69d22ea8ad134feaca
ChrisOtt0 <sophusott@gmail.com>
2022-11-21 16:09:09 +0100

    Updated missing DbSets, added Create Group and Read Groups

--- Controllers/GroupController.cs
+++ Controllers/GroupController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace webapiSBIFS.Controllers
@@ -16,6 +17,28 @@ namespace webapiSBIFS.Controllers
             _userService = userService;
         }
 
+        [HttpGet(Name = "ReadGroups"), Authorize(Roles = "user")]
+        public async Task<ActionResult<List<Group>>> GetGroups()
+        {
+            int userID = _userService.GetUserID();
+            List<Group> groups = await _context.Groups.Where(g => g.OwnerID == userID).ToListAsync();
+            return Ok(groups);
+        }
 
+        [HttpPost(Name = "Create"), Authorize(Roles = "user")]
+        public async Task<ActionResult<List<Group>>> Create()
+        {
+            int userID = _userService.GetUserID();
+
+            Group g = new Group();
+            g.OwnerID = userID;
+
+            await _context.Groups.AddAsync(g);
+            await _context.SaveChangesAsync();
+
+            List<Group> groups = await _context.Groups.Where(g => g.OwnerID == userID).ToListAsync();
+
+            return new ObjectResult(groups) { StatusCode = StatusCodes.Status201Created };
+        }
     }
 }
